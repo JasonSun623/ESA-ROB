f196f67422eab4040981ddf99d20820243e88cb6
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-09-04 12:59:12 +0200

    Dingen2

--- assignment1/src/draw_triangle_node.cpp
+++ assignment1/src/draw_triangle_node.cpp
@@ -1,10 +1,9 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/String.h>
+
 
 #include <sstream>
-
-float sideLength;
-bool cw;
 
 void cmdCallback(const std_msgs::String::ConstPtr& msg)
 {
@@ -17,26 +16,19 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "draw_triangle_node");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("cmd", 1000, cmdCallback);
+  ros::Publisher vel_pub_ = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-  /*while (ros::ok())
+  while (ros::ok())
   {
-    std_msgs::String msg;
+  	geometry_msgs::Twist vel;
+    vel.angular.z = 1;
+    vel.linear.x = 1;
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    cmd_sub.publish(msg);
-
+    vel_pub_.publish(vel);    
     ros::spinOnce();
+  }
 
-    loop_rate.sleep();
-    ++count;
-  }s
-*/
-  ros.spin();
+  //ros::spin();
 
   return 0;
 }
