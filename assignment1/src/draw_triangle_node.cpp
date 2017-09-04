16caa5ec9cd8791eb5d30c9f762ae3516104da98
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-09-04 12:22:25 +0200

    Fix build things

--- assignment1/src/draw_triangle_node.cpp
+++ assignment1/src/draw_triangle_node.cpp
@@ -1,15 +1,42 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
 
-void cmdCallback(const std_msgs::String::ConstPtr& msg) {
-	
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
+#include <sstream>
+
+float sideLength;
+bool cw;
+
+void cmdCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv) {
-	ros::init(argc, argv, "listener");
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("cmd", 1000, cmdCallback);
-	ros::spin();
-	return 0;
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "draw_triangle_node");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("cmd", 1000, cmdCallback);
+
+  /*while (ros::ok())
+  {
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    cmd_sub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }s
+*/
+  ros.spin();
+
+  return 0;
 }
