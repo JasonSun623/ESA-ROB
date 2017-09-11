1ecc667e5d839e52d05421ae06e02f2d1d8688a4
Lars Jaeqx <lars.jaeqx@gmail.com>
2017-09-11 03:08:53 -0500

    created frame

--- assignment1/src/draw_triangle_node.cpp
+++ assignment1/src/draw_triangle_node.cpp
@@ -2,29 +2,37 @@
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #include <assignment1/Triangle.h>
-
-
 #include <sstream>
 
-void cmdCallback(const assignment1::Triangle::ConstPtr& msg)
-{
+void turn() {
+
+}
+
+void forward() {
+
+}
+
+void timerCallback() {
+	
+}
+
+void cmdCallback(const assignment1::Triangle::ConstPtr& msg) {
   ROS_INFO("I heard: [%f]", msg->sideLength);
   ROS_INFO("I heard: [%d]", msg->cw);
 }
 
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "draw_triangle_node");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("cmd", 1000, cmdCallback);
   ros::Publisher vel_pub_ = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-  while (ros::ok())
-  {
+  ros::Timer timer = n.createTimer(ros::Duration(0.016), boost::bind(timerCallback, _1, twist_pub));
+
+  while (ros::ok()) {
   	geometry_msgs::Twist vel;
-    vel.angular.z = 1;
-    vel.linear.x = 1;
+    vel.angular.z = 5;
+    vel.linear.x = 0;
 
     vel_pub_.publish(vel);    
     ros::spinOnce();
