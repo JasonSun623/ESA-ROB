5790107fbccdbe9927e84eb0cba657d1b41911ab
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-09-25 11:54:06 +0200

    Add angle calculation to p/s

--- src/assignment2/readme.md
+++ src/assignment2/readme.md
@@ -3,12 +3,16 @@ ESA-ROS Assignment 2
 
 Assignment 2 with moves etc.
 
+Dingen die handig zijn om te doen:
+* Comment in de code waar de code vandaan komt
+* Meer testen
+* Uitleg _waarom_ we denken wat er gebeurt
+
 # Code explanation
 
 # Running instructions
 
-# Observations
+# Tests and Observations
 
-# Graph etc
-
+# Graph 
 
--- src/assignment2/src/pointshoot_node.cpp
+++ src/assignment2/src/pointshoot_node.cpp
@@ -1,13 +1,40 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <nav_msgs/Odometry.h>
+#include <math.h>
 
+ros::Publisher pubVelCmd;
+geometry_msgs::Pose currentPose;
+
+double getAngleBetweenPoses2D(geometry_msgs::Pose p1, geometry_msgs::Pose p2) {
+    float dx = p2.position.x - p1.position.x;
+    float dy = p2.position.y - p1.position.y;
+    return (atan2(dx, dy));
+}
+
+void cbGoal(const geometry_msgs::PoseStamped::ConstPtr &msg) {
+    geometry_msgs::Pose goal = msg->pose;
+
+    double angle = getAngleBetweenPoses2D(currentPose, goal);
+
+	ROS_INFO("angle: %lf\n", angle);
+    
+}
+
+void cbOdom(const nav_msgs::Odometry::ConstPtr &msg) {
+    currentPose = msg->pose.pose;
+
+}
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "pointshoot_node");
 	ros::NodeHandle n;
-    
-    /*
-     * More init
-     */
+    ros::Subscriber subGoal = n.subscribe("/goal", 100, cbGoal);
+    ros::Subscriber subOdom = n.subscribe("/odom", 100, cbOdom);
+    pubVelCmd = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
     
 	ros::spin();
 
