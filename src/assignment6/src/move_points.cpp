8490b96aff5b6adf7aef0b2d09d67cdc785ba904
mt <minhtrietdiep@gmail.com>
2017-12-18 12:46:08 +0100

    fiks

--- src/assignment6/src/move_points.cpp
+++ src/assignment6/src/move_points.cpp
@@ -4,11 +4,9 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-std::vector <move_base_msgs::MoveBaseGoal> createNavPoints()
-{
+std::vector <move_base_msgs::MoveBaseGoal> createNavPoints() {
     std::vector <move_base_msgs::MoveBaseGoal> goals;
-    for(auto i; i < 3.0; i++)
-    {
+    for(double i; i < 3.0; i++) {
         move_base_msgs::MoveBaseGoal goal;
 
          //we'll send a goal to the robot to move 1 meter forward
@@ -16,54 +14,55 @@ std::vector <move_base_msgs::MoveBaseGoal> createNavPoints()
         goal.target_pose.header.stamp = ros::Time::now();
 
         goal.target_pose.pose.position.x = i;
-        goal.target_pose.actionlib_msgspose.orientation.w = i; 
+        goal.target_pose.pose.orientation.w = 1;
 
-        goals.pushback(goal);
+        //goal.target_pose.actionlib_msgs.pose.orientation.w = i/3.0; 
+
+        goals.push_back(goal);
     }
 
     return goals;
 }
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "simple_navigation_goals");
 
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
+    //tell the action client that we want to spin a thread by default
+    MoveBaseClient ac("move_base", true);
 
-  //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-  
-  std::vector <move_base_msgs::MoveBaseGoal> goals = createNavPoints();
+    //wait for the action server to come up
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goals[0]);
+    std::vector <move_base_msgs::MoveBaseGoal> goals = createNavPoints();
 
-  ac.waitForResult();
-
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  {
-    ROS_INFO("Hooray");
-      ac.sendGoal(goals[1]);
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goals[0]);
 
     ac.waitForResult();
-  
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    {
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         ROS_INFO("Hooray");
-        ac.sendGoal(goals[2]);
+        ac.sendGoal(goals[1]);
 
         ac.waitForResult();
-        if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        {
-            ROS_INFO("Hooray");
-        }
-  }
-  else
-  {
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
-  }
 
-  return 0;
+        if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+            ROS_INFO("Hooray");
+            ac.sendGoal(goals[2]);
+
+            ac.waitForResult();
+            if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+                ROS_INFO("Hooray");
+            }
+        }
+
+
+    }
+    else {
+        ROS_INFO("The base failed to move forward 1 meter for some reason");
+    }
+    return 0;
+
 }
